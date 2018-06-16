f788d43c0a3b62d4a0ec1daccc14c3be893d936b
riv4wi <riv4wi@gmail.com>
2018-06-16 01:13:00 -0300

    Created interface for ApiRest class

---
+++ core/iApiRest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace sbApi;
+
+/**
+ * iApiRest Interface
+ *
+ * Define each of the functions that the ApiRest.php must have
+ *
+ * @author riv4wi
+ * @version 1.0
+ */
+interface iApiRest
+{
+    // GET : Get an element
+    public function get();
+
+    // POST : Insert an element
+    public function post();
+
+    // PUT: Update an element
+    public function put();
+
+    // DELETE: Delete an element
+    public function delete();
+}
