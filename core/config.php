cd6e51611c68ee38efa2c23e6192e3b987207252
riv4wi <riv4wi@gmail.com>
2018-06-16 01:11:53 -0300

    Created file's configuration, especially parameters of connection to DB

---
+++ core/config.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace sbApi;
+
+/**
+ * Configuration of the connection to database.
+ *
+ * In this file the API configuration information will be defined
+ * variables, constants and functions required for the rest of the files
+ *
+ * @author riv4wi
+ * @version 1.0
+ */
+
+// Database server address
+define("DB_HOST", "localhost");
+
+// Database user name
+define("DB_USER", "riv4wi");
+
+// Database user password
+define("DB_PASS", "1234");
+
+// Database name
+define("DB_NAME", "scatchbling");
